e2759ae8dd0a0b20c82492ed34b6c7f04d16beb3
rajat29gupta <rajat29gupta@gmail.com>
2019-05-06 16:03:15 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# pdbenmbryey.angular
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/angular/pdbenmbryey)
---
+++ app/cdk-drag-drop-connected-sorting-example.ts
@@ -0,0 +1,40 @@
+import {Component} from '@angular/core';
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+
+/**
+ * @title Drag&Drop connected sorting
+ */
+@Component({
+  selector: 'cdk-drag-drop-connected-sorting-example',
+  templateUrl: 'cdk-drag-drop-connected-sorting-example.html',
+  styleUrls: ['cdk-drag-drop-connected-sorting-example.css'],
+})
+export class CdkDragDropConnectedSortingExample {
+  todo = [
+    'Form 1',
+    'Form 2',
+    'Form 3',
+    'Form 4'
+  ];
+
+  done = [
+    'Form 5',
+    'Form 6',
+  ];
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+                        event.container.data,
+                        event.previousIndex,
+                        event.currentIndex);
+    }
+  }
+}
+
+
+/**  Copyright 2019 Google Inc. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
